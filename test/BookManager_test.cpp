73a4e02e55ee4fa6eb97fada32bc3974223a7ece
juansebasdev <jbravomeneses@gmail.com>
2023-10-15 12:33:26 -0500

    feat: ci for book manager
    	- Add tests for application

---
+++ test/BookManager_test.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+#include "Book.h"
+#include "BookStore.h"
+
+TEST(BookTest, Create)
+{
+    std::string title = "Fairytale";
+    std::string description = "A great Adventure";
+
+    Book book = Book(Book::currentId, title, description);
+
+    ASSERT_EQ(book.GetTitle(), title);
+    ASSERT_EQ(book.GetDescription(), description);
+}
+
+TEST(BookStoreTest, Save)
+{
+    BookStore bookStore;
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
